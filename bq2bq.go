7c932bbd85366143fba18c0383be0bc374285a10
Eugenio Contreras <ecs.keni@gmail.com>
2022-06-19 17:25:23 -0400

    Add DoFn and ParDo functions

--- bq2bq.go
+++ bq2bq.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/apache/beam/sdks/v2/go/pkg/beam"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/io/bigqueryio"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam/io/textio"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/log"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/options/gcpopts"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/x/beamx"
@@ -17,6 +20,22 @@ type Row struct {
 }
 
 const query = ``
+const delimiter = ";"
+
+func extractFn(line string, delimiter string, emit func([]string)) {
+	split := strings.Split(line, delimiter)
+	emit(split)
+}
+
+func SplitLines(s beam.Scope, lines beam.PCollection, delimiter string) beam.PCollection {
+	s = s.Scope("Split Lines")
+	col := beam.ParDo(s, extractFn, lines)
+	return col
+}
+
+func formatFn(w string) string {
+	return fmt.Sprintf("%s", w)
+}
 
 func main() {
 
@@ -34,6 +53,11 @@ func main() {
 
 	rows := bigqueryio.Query(s, project, query,
 		reflect.TypeOf(Row{}), bigqueryio.UseStandardSQL())
+
+	lines := SplitLines(s, rows, delimiter)
+	formatted := beam.ParDo(s, formatFn, lines)
+
+	textio.Write(s, "/tmp/output.csv", formatted)
 
 	bigqueryio.Write(s, project, "", rows)
 }
